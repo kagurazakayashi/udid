b5a0aab6a29b43b7bf4078eba7b824d1d287e0f4
神楽坂雅詩 <cyscxchope@gmail.com>
2022-02-20 15:12:24 +0800

    完成前端

--- info.php
+++ info.php
@@ -29,6 +29,8 @@ function chkArg(): bool {
 function loadFile(): string|false {
     $fileName = $_GET['f'];
     if (!file_exists($fileName)) {
+        header('content-type:text/html;charset="utf-8"');
+        echo '链接已过期';
         http_response_code(404);
         return false;
     }
@@ -84,6 +86,11 @@ function findKeyValue(array $arr) {
 if (!chkArg()) {
     return;
 }
+if (in_array('delete', array_keys($_GET))) {
+    @unlink($_GET['f']);
+    header('content-type:text/html;charset="utf-8"');
+    exit('已永久删除服务器上的记录');
+}
 $d = loadFile();
 if (!$d) {
     return;
@@ -93,5 +100,63 @@ $d = subString($d, '<dict>', '</dict>');
 $da = explode("\n", $d);
 $da = clearEmpty($da);
 $da = findKeyValue($da);
-header('Content-type:application/json');
-echo json_encode($da);
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0,minimal-ui:ios">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#f2f1f6">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#f2f1f6">
+    <meta name="format-detection" content="telephone=no">
+    <title>我的设备</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <p>&emsp;</p>
+    <div class="groupTitle key">我的设备信息</div>
+    <div class="group">
+        <div class="item">
+            <?php
+            $first = true;
+            foreach ($da as $key => $value) {
+                if ($first) {
+                    $first = false;
+                } else {
+                    echo '<hr/>';
+                }
+                echo "<div class=\"val\">$value</div>";
+                echo "<div class=\"key\">$key</div>";
+            }
+            ?>
+        </div>
+    </div>
+    <div class="groupTitle key">长按以复制文本。</div>
+    <p>&emsp;</p>
+    <div class="groupTitle key">不会永久存储任何您的设备信息</div>
+    <div class="group">
+        <div class="item">
+            <a href="<?php echo 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING'] . '&delete'; ?>" target="_self">
+                <div class="val">删除服务器上的记录</div>
+                <div class="key">本链接将会失效</div>
+            </a>
+            <hr />
+            <a href="index.html" target="_self">
+                <div class="val">重新获取</div>
+                <div class="key">重新安装描述文件</div>
+            </a>
+            <hr />
+            <a href="https://github.com/kagurazakayashi/udid" target="_blank">
+                <div class="val">开源代码</div>
+                <div class="key">访问 Github 仓库</div>
+            </a>
+        </div>
+    </div>
+    <div class="groupTitle key">by 神楽坂雅詩</div>
+    <p>&emsp;</p>
+</body>
+
+</html>
